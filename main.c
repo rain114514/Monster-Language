1d28fdfb807a25fbc256e53ee82da830d27114b1
rain114514 <2107665369@qq.com>
2025-01-29 12:00:56 +0800

    完成了第一次设计

--- main.c
+++ main.c
@@ -10,8 +10,11 @@ QList GetString();
 int IsLegal(char ch);
 int CheckString(QList Q);
 void ErrorPrint(QList Q, int E);
+QList Translate1(QList Q);
+void Translate(QList Q);
 
 int main() {
+    //主函数
     QList Q; //存储字符串
     int flag;
 
@@ -19,6 +22,9 @@ int main() {
     flag = CheckString(Q); //合法性检查
     if (flag != 1) ErrorPrint(Q, flag); //显示字符串的非法之处
     else printf("The string is legal!\n");
+    Translate(Q);
+
+    return 0;
 } //main
 
 QList GetString() {
@@ -117,3 +123,58 @@ void ErrorPrint(QList Q, int E) {
         } default: break;
     } //switch
 } //ErrorPrint
+
+QList Translate1(QList Q) {
+    //翻译字符串Q内的所有括号(不替换魔王词汇)
+    QList NQ;
+    SList S;
+    QNode p = Q->front->next;
+    char ch, ch0;
+
+    NQ = InitQueue();
+    S = InitStack();
+    while (p != Q->rear) {
+        if (p->data == '(') { //遇到左括号，开始处理括号
+            ch = p->next->data;
+            p = p->next->next;
+            while (p->data != ')') { //括号结束前
+                Push(S, p->data);
+                p = p->next;
+            } //while
+            EnQueue(NQ, ch);
+            while (!StackEmpty(S)) { //按规则实现
+                ch0 = Pop(S);
+                EnQueue(NQ, ch0);
+                EnQueue(NQ, ch);
+            } //while
+        } else if (IsLegal(p->data)) {
+            //不在括号内，直接入队
+            EnQueue(NQ, p->data);
+        } //if
+        p = p->next;
+    } //while
+    DestroyQueue(Q); //原先的字符串释放掉
+
+    return NQ;
+} //Translate1
+
+void Translate(QList Q) {
+    //翻译，先进行括号处理，随后替换
+    QList NQ;
+    QNode p;
+
+    NQ = Translate1(Q);
+    p = NQ->front->next;
+    while (p != Q->rear) {
+        if (p->data >= 'a' && p->data <= 'z') {
+            printf("%c", p->data);
+        } else if (p->data == 'A') { //替换'A'
+            printf("sae");
+        } else if (p->data == 'B') { //替换'B'
+            printf("tsaedsae");
+        } //if
+        p = p->next;
+    } //while
+    printf("\nSucceed to translate!\n");
+    DestroyQueue(NQ); //销毁字符串
+} //Translate
