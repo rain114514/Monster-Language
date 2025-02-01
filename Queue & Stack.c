3d924379961b90c18f7614a0b078898634dcebf2
rain114514 <2107665369@qq.com>
2025-02-01 12:38:17 +0800

    完成了链式队列的设计

--- Queue & Stack.c
+++ Queue & Stack.c
@@ -21,7 +21,7 @@
 typedef struct Node {
     Elemtype data;
     struct Node *next;
-} Node, *QNode, *SNode; //链式结构的基础
+} Node, *QNode, *SNode; //链式节点定义
 
 typedef struct Stack {
     SNode top;
@@ -42,10 +42,9 @@ Status InitNode(Node *N) {
 
 Status InitStack(SList S) {
     //创建一个空的链式栈S并返回
-    S = (SList)malloc(sizeof(SNode));
-    if (S == NULL) return OVERFLOW;
-    InitNode(S->top); //为栈顶申请空间
-    if (InitNode(S->top) == OVERFLOW) return OVERFLOW;
+    S = (SList)malloc(sizeof(Stack));
+    if (S == NULL) return OVERFLOW; //申请失败
+    if (InitNode(S->top) == OVERFLOW) return OVERFLOW; //栈顶申请失败
     S->top->next = NULL; //空链式栈中，top->next为NULL
 
     return OK;
@@ -53,7 +52,7 @@ Status InitStack(SList S) {
 
 Status StackEmpty(SList S) {
     //判断链式栈S是否为空
-    if (S == NULL) return ERROR;
+    if (S == NULL) return ERROR; //链式栈S不存在
     if (S->top->next == NULL) return TRUE;
 
     return FALSE;
@@ -64,12 +63,12 @@ Status Push(SList S, Elemtype x) {
     SNode p;
     int flag;
 
-    if (S == NULL) return ERROR;
+    if (S == NULL) return ERROR; //链式栈S不存在
     if (InitNode(p) == OK) { //新节点申请成功
         p->data = x;
         p->next = S->top->next;
-        S->top->next = p; //将新节点p插入top后
-    } else return OVERFLOW;
+        S->top->next = p; //将新节点p插在top后
+    } else return OVERFLOW; //新节点申请失败
 
     return OK;
 } //Push
@@ -78,8 +77,8 @@ Status Pop(SList S, Elemtype *x) {
     //将链式栈S的栈顶元素弹出，并返回它的元素值
     SNode p;
 
-    if (S == NULL) return ERROR;
-    if (StackEmpty(S)) return INFEASIBLE;
+    if (S == NULL) return ERROR; //链式栈S不存在
+    if (StackEmpty(S)) return INFEASIBLE; //链式栈S为空栈
     p = S->top->next;
     *x = p->data; //取出栈顶元素值
     S->top->next = p->next;
@@ -92,17 +91,74 @@ Status DestroyStack(SList S) {
     //彻底销毁链式栈S
     Elemtype x;
 
-    if (S == NULL) { //链式栈不存在
-        free(S);
-
-        return OK;
-    } //if
-    while (!StackEmpty(S)) { //逐个释放节点
-        Pop(S, &x);
-    } //while
+    if (S == NULL) return OK;
+    while (!StackEmpty(S)) Pop(S, &x);
     free(S->top);
     free(S); //彻底销毁链式栈的结构
     S = NULL;
 
     return OK;
 } //DestroyStack
+
+Status InitQueue(QList Q) {
+    //创建一个空的链式队列Q并返回
+    Q = (QList)malloc(sizeof(Queue));
+    if (Q == NULL) return OVERFLOW; //申请失败
+    if (InitNode(Q->front) == OVERFLOW) return OVERFLOW; //队头申请失败
+    if (InitNode(Q->rear) == OVERFLOW) return OVERFLOW; //队尾申请失败
+    Q->front->next = Q->rear; //空的链式队列中，front->next为rear
+
+    return OK;
+} //InitQueue
+
+Status QueueEmpty(QList Q) {
+    //判断链式队列Q是否为空
+    if (Q == NULL) return ERROR; //链式队列Q不存在
+    if (Q->front->next == Q->rear) return TRUE;
+
+    return FALSE;
+} //QueueEmpty
+
+Status EnQueue(QList Q, Elemtype x) {
+    //将元素x推入链式队列Q
+    QNode p, q;
+
+    if (Q == NULL) return ERROR; //链式队列Q不存在
+    if (InitNode(p) == OK) { //新节点申请成功
+        p->data = x;
+        q = Q->front; //找到rear前一个节点
+        while (q->next != Q->rear) q = q->next;
+        p->next = Q->rear;
+        q->next = p; //将新节点p插入rear前
+    } else return OVERFLOW; //新节点申请失败
+
+    return OK;
+} //EnQueue
+
+Status DeQueue(QList Q, Elemtype *x) {
+    //将链式队列Q的队头元素取出，并返回它的元素值
+    QNode p;
+
+    if (Q == NULL) return ERROR; //链式队列Q不存在
+    if (QueueEmpty(Q)) return INFEASIBLE; //链式队列Q为空
+    p = Q->front->next;
+    *x = p->data; //取出队头元素值
+    Q->front->next = p->next;
+    free(p); //将原先的栈顶节点释放
+
+    return OK;
+} //DeQueue
+
+Status DestroyQueue(QList Q) {
+    //将链式队列Q彻底销毁
+    Elemtype x;
+
+    if (Q == NULL) return OK;
+    while (!QueueEmpty(Q)) Pop(Q, &x);
+    free(Q->front);
+    free(Q->rear);
+    free(Q); //彻底销毁链式队列Q的结构
+    Q = NULL;
+
+    return OK;
+} //DestroyQueue
